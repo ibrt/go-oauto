b7dd4ab85ff2d106c14b47b75caf5c6e79ade769
Ivan Bertona <ivan.bertona@gmail.com>
2016-03-09 17:32:05 -0800

    Add client and basic test.

---
+++ oauto/client/authenticate.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"github.com/ibrt/go-oauto/oauto/api"
+	"net/http"
+	"fmt"
+	"encoding/json"
+	"github.com/go-errors/errors"
+	"bytes"
+)
+
+func Authenticate(baseURL string, request *api.AuthenticateRequest) (*api.AuthenticateResponse, error) {
+	body, err := json.Marshal(request)
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+
+	resp, err := http.Post(fmt.Sprintf("%v/api/authenticate", baseURL), "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("Authenticate request failed with status %v.", resp.StatusCode)
+	}
+
+	authResp := &api.AuthenticateResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(&authResp); err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+
+	return authResp, nil
+}
---
+++ oauto/oauto_test.go
@@ -0,0 +1,40 @@
+package oauto_test
+
+import (
+	"testing"
+	"github.com/kelseyhightower/envconfig"
+	"os"
+	"github.com/ibrt/go-oauto/oauto/api"
+	"github.com/ibrt/go-oauto/oauto/client"
+)
+
+type TestConfig struct {
+	BaseURL string `envconfig:"BASE_URL" required:"true"`
+	FacebookAppID string `envconfig:"FACEBOOK_APP_ID" required:"true"`
+	FacebookUserName string `envconfig:"FACEBOOK_USER_NAME" required:"true"`
+	FacebookPassword string `envconfig:"FACEBOOK_PASSWORD" required:"true"`
+}
+
+var testConfig = &TestConfig{}
+
+func TestMain(m *testing.M) {
+	envconfig.MustProcess("OAUTO_TEST", testConfig)
+	os.Exit(m.Run())
+}
+
+func TestFacebook(t *testing.T) {
+	resp, err := client.Authenticate(
+		testConfig.BaseURL,
+		&api.AuthenticateRequest{
+			Provider: "facebook",
+			AppID: testConfig.FacebookAppID,
+			UserName: testConfig.FacebookUserName,
+			Password: testConfig.FacebookPassword,
+		})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resp.Token) == 0 {
+		t.Fatal("Missing token in Facebook authentication response.")
+	}
+}
